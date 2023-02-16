c6bcbeb30411e545aff9ea344ece8624d3d702be
ge3224 <ge3224@gmail.com>
2023-02-16 14:30:17 -0600

    Cleanup documentation in src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,42 @@
-//! A Neocities API client written in Rust
-//!
-//! # Installation:
-//!
-//! // TODO
-//!
-//! # Usage:
-//!
-//! - Upload files to your website:
-//!
-//! ```bash
-//! $ neocities upload foo.html bar.js folder/baz.jpg
-//! ```
-//! - Delete files from your website:
-//!
-//! ```bash
-//! $ neocities delete foo.html folder/baz.jpg
-//! ```
-//! Get a list of available commands:
-//!
-//! ```bash
-//! $ neocities
-//!
-//! // output
-//! usage: neocities <command> [<args>]
-//!
-//! Commands:
-//!    upload    Upload files to Neocities
-//!    delete    Delete files from Neocities
-//!    info      Info about Neocities websites
-//!    key       Neocities API key
-//!    list      List files on Neocities
-//!    version   Show neocities client version
-//!
-//!  Help for a specific command:
-//!    help [command]
-//! ```
+/*! 
+ A Neocities API client written in Rust
+
+ # Installation:
+
+ // TODO
+
+ # Usage:
+
+ - Upload files to your website:
+
+ ```bash
+ $ neocities upload foo.html bar.js folder/baz.jpg
+ ```
+ - Delete files from your website:
+
+ ```bash
+ $ neocities delete foo.html folder/baz.jpg
+ ```
+ Get a list of available commands:
+
+ ```bash
+ $ neocities
+
+ // output
+ usage: neocities <command> [<args>]
+
+ Commands:
+    upload    Upload files to Neocities
+    delete    Delete files from Neocities
+    info      Info about Neocities websites
+    key       Neocities API key
+    list      List files on Neocities
+    version   Show neocities client version
+
+  Help for a specific command:
+    help [command]
+ ```
+*/
 
 pub mod api;
 pub use api::Credentials;
