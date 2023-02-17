3cf0228c8cba0b5c805fce102cb160b7c1757bc5
ge3224 <ge3224@gmail.com>
2023-02-17 17:28:27 -0600

    Add Upload module

---
+++ src/client/upload.rs
@@ -0,0 +1,44 @@
+use super::command::Executable;
+
+pub const UP: &'static str = "upload";
+
+pub struct Upload {
+    key: String,
+    usage: String,
+    short: String,
+    long: String,
+}
+
+impl Upload {
+    pub fn new() -> Upload {
+        Upload {
+            key: String::from("upload"),
+            usage: String::from("upload <filename> [<another filename>]"),
+            short: String::from("Upload files to Neocities"),
+            long: String::from("Upload files to your Neocities website"),
+        }
+    }
+}
+
+impl Executable for Upload {
+    fn run(&self, args: Vec<String>) -> Result<(), &'static str> {
+        println!("Implementation of Executable for Upload");
+        Ok(())
+    }
+
+    fn get_key(&self) -> &str {
+        self.key.as_str()
+    }
+
+    fn get_usage(&self) -> &str {
+        self.usage.as_str()
+    }
+
+    fn get_short_desc(&self) -> &str {
+        self.short.as_str()
+    }
+
+    fn get_long_desc(&self) -> &str {
+       self.long.as_str() 
+    }
+}
