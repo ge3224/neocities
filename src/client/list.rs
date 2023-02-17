0c77372bcabcf6c2c58974360a9c1beb3fa13d76
ge3224 <ge3224@gmail.com>
2023-02-17 17:28:14 -0600

    Add List module

---
+++ src/client/list.rs
@@ -0,0 +1,44 @@
+use super::command::Executable;
+
+pub const LIST: &'static str = "list";
+
+pub struct List {
+    key: String,
+    usage: String,
+    short: String,
+    long: String,
+}
+
+impl List {
+    pub fn new() -> List {
+        List {
+            key: String::from("list"),
+            usage: String::from("list"),
+            short: String::from("List files on Neocities"),
+            long: String::from("List files in your Neocities website"),
+        }
+    }
+}
+
+impl Executable for List {
+    fn run(&self, args: Vec<String>) -> Result<(), &'static str> {
+        println!("List's implementation of Executable: {:?}", args);
+        Ok(())
+    }
+
+    fn get_key(&self) -> &str {
+        self.key.as_str()
+    }
+
+    fn get_usage(&self) -> &str {
+        self.usage.as_str()
+    }
+
+    fn get_short_desc(&self) -> &str {
+        self.short.as_str()
+    }
+
+    fn get_long_desc(&self) -> &str {
+        self.long.as_str()
+    }
+}
