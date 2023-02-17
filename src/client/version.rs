0d9fa95b395807a2c7db4e4e1bff9f24d083148d
ge3224 <ge3224@gmail.com>
2023-02-17 17:28:41 -0600

    Add Version module

---
+++ src/client/version.rs
@@ -0,0 +1,44 @@
+use super::command::Executable;
+
+pub const VER: &'static str = "version";
+
+pub struct Version {
+    key: String,
+    usage: String,
+    short: String,
+    long: String,
+}
+
+impl Version {
+    pub fn new() -> Version {
+        Version {
+            key: String::from("version"),
+            usage: String::from("version"),
+            short: String::from("Show neocities version"),
+            long: String::from("Show the version number of the neocities client"),
+        }
+    }
+}
+
+impl Executable for Version {
+    fn run(&self, args: Vec<String>) -> Result<(), &'static str> {
+        println!("Version' implementation of Executable: {:?}", args);
+        Ok(())
+    }
+
+    fn get_key(&self) -> &str {
+        self.key.as_str()
+    }
+
+    fn get_usage(&self) -> &str {
+        self.usage.as_str()
+    }
+
+    fn get_short_desc(&self) -> &str {
+        self.short.as_str()
+    }
+
+    fn get_long_desc(&self) -> &str {
+        self.long.as_str()
+    }
+}
