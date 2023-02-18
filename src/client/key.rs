191161fad18544147641bffa2f1bf72b9f20a112
ge3224 <ge3224@gmail.com>
2023-02-17 18:01:07 -0600

    Add Key module

---
+++ src/client/key.rs
@@ -0,0 +1,44 @@
+use super::command::Executable;
+
+pub const KEY: &'static str = "key";
+
+pub struct Key {
+    key: String,
+    usage: String,
+    short: String,
+    long: String,
+}
+
+impl Key {
+    pub fn new() -> Key {
+        Key {
+            key: String::from(KEY),
+            usage: String::from(KEY),
+            short: String::from("Neocities API Key"),
+            long: String::from("Retrieve an API Key for your Neocities user"),
+        }
+    }
+}
+
+impl Executable for Key {
+    fn run(&self, _args: Vec<String>) -> Result<(), &'static str> {
+        println!("TODO: Key run");
+        Ok(())
+    }
+
+    fn get_key(&self) -> &str {
+        self.key.as_str()
+    }
+
+    fn get_usage(&self) -> &str {
+        self.usage.as_str()
+    }
+
+    fn get_short_desc(&self) -> &str {
+        self.short.as_str()
+    }
+
+    fn get_long_desc(&self) -> &str {
+        self.long.as_str()
+    }
+}
