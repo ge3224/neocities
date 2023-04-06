b3c4b34cf9d9058b5925f432489235a4eb757905
Jacob <ge3224@gmail.com>
2023-04-05 22:06:20 -0500

    Utilize NeocitiesErr in runner mod

--- src/client/runner.rs
+++ src/client/runner.rs
@@ -1,10 +1,9 @@
-use std::error::Error;
-
 use super::config::Config;
+use crate::error::NeocitiesErr;
 
 /// Runs the `neocities_cli` application, based on a configuration argument. It returns either a
 /// zero-sized type or an error.
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), NeocitiesErr> {
     config.use_command()?;
     Ok(())
 }
