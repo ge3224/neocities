d363729b76ee0f57eafc0e1ad717ac9b2648121a
Jacob <ge3224@gmail.com>
2023-08-27 23:51:50 -0500

    Add documentation to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+use neocities_cli::client::{config::Config, runner::run};
 use std::{env, process};
 
-use neocities_cli::client::{runner::run, config::Config};
-
+// The main entry point of the program.
 fn main() {
+    // Collect command-line arguments into a vector of strings.
     let input: Vec<String> = env::args().collect();
 
+    // Build a configuration using the input arguments.
     let config = Config::build(&input);
 
+    // Attempt to run the program with the provided configuration.
     if let Err(e) = run(config) {
+        // Print an error message in bold text, followed by the error details.
         eprintln!("\x1b[1mError: \x1b[0m{e}");
+
+        // Exit the program with a non-zero status code to indicate an error.
         process::exit(1);
     }
 }
